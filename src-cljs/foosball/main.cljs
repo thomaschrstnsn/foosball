e75d7ff47610371ae6ffeeb9797c5c94fc5cbabb
Thomas Christensen <thomas@chrstnsn.dk>
2013-05-11 09:29:33 +0200

    set current element in navbar active using cljs

--- src-cljs/foosball/main.cljs
+++ src-cljs/foosball/main.cljs
@@ -1,11 +1,29 @@
 (ns foosball.main
   (:use-macros [dommy.macros :only [sel sel1 nodes]])
-  (:require [dommy.core :as dommy]))
+  (:require [dommy.core :as dommy]
+            [clojure.browser.repl :as repl]))
 
 (defn log [& items]
   (.log js/console (apply str (interpose ", " items))))
 
-(defn ^:export set-active-navbar! []
-  (js/alert "hey: you")
-  (log (->> (sel [".nav" :li])
-            (map log))))
+(defn current-path []
+  window.location.pathname)
+
+(defn navbar-elements []
+  (sel [".nav" :li]))
+
+(defn current-navbar-element []
+  (->> (navbar-elements)
+       (filter (fn [e] (-> (dommy/html e)
+                          (.indexOf (current-path))
+                          (>= 0))))
+       first))
+
+(defn set-active-navbar-element! []
+  (let [el (current-navbar-element)]
+    (when el
+      (dommy/add-class! el :active))))
+
+(defn ^:export page-loaded []
+  ;(repl/connect "http://localhost:9000/repl")
+  (set-active-navbar-element!))
