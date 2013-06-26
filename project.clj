fb7a3400c89d182788eed3bbf189b7d1f43c9d25
Thomas Christensen <thomas@chrstnsn.dk>
2013-06-26 23:28:00 +0200

    lein-release plugin: bumped version from 0.9.7 to 0.9.8-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject foosball "0.9.7"
+(defproject foosball "0.9.8-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.datomic/datomic-free "0.8.3941" :exclusions [com.amazonaws/aws-java-sdk]]
                  [lib-noir "0.6.4"]
