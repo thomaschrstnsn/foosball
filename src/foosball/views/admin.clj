1d2aa115ae997cbd61eefd1291f5accbbcb83694
Thomas Christensen <thomas@chrstnsn.dk>
2013-08-30 07:12:47 +0200

    styling of admin page with bootstrap 3 styles

--- src/foosball/views/admin.clj
+++ src/foosball/views/admin.clj
@@ -7,28 +7,30 @@
         [taoensso.timbre :only [trace debug info warn error fatal spy]]))
 
 (defn- player-select [action-uri header-label button-label class players]
-  [:form {:action action-uri :method "POST"}
-    [:div.input-append
-     [:h1 header-label]
-     [:select.col-lg-3 {:name "playerid"}
+  [:form.form-inline {:action action-uri :method "POST"}
+   [:h1 header-label]
+   [:div.form-group.col-lg-6
+     [:select.form-control {:name "playerid"}
       [:option "Player"]
       (->> players
-           (map (fn [{:keys [id name]}] [:option {:value id} name])))]
-     [:button.btn {:class class :type "submit" :value "value"} button-label]]])
+           (map (fn [{:keys [id name]}] [:option {:value id} name])))]]
+   [:div.form-group.col-lg-6
+    [:button.btn {:class class :type "submit" :value "value"} button-label]]])
 
 (defn form [players matches]
   (html5
-   [:form {:action "/admin/player/add" :method "POST"}
-    [:div.input-append
+   [:div
+    [:form.form-inline {:action "/admin/player/add" :method "POST"}
      [:h1 "Add player"]
-     [:input.col-lg-3 {:type "text" :name "playername" :placeholder "New Player"}]
-     [:button.btn.btn-primary {:type "submit" :value "Report"} "Add!"]]]
+     [:div.form-group.col-lg-6
+      [:input.form-control.col-lg-3 {:type "text" :name "playername" :placeholder "New Player"}]]
+     [:div.form-group.col-lg-6
+      [:button.btn.btn-primary {:type "submit" :value "Report"} "Add!"]]]
 
-   (player-select "/admin/player/activate" "Activate player" "Set active" "btn-success"
-                  (filter (comp not :active) players))
+    (player-select "/admin/player/activate" "Activate player" "Set active" "btn-success"
+                   (filter (comp not :active) players))
+    (player-select "/admin/player/deactivate" "Deactivate player" "Set inactive" "btn-danger"
+                   (filter :active players))
 
-   (player-select "/admin/player/deactivate" "Deactivate player" "Set inactive" "btn-danger"
-                  (filter :active players))
-
-   [:form {:action "/admin/match/remove" :method "POST"}
-    (match-table-data matches players :admin true)]))
+    [:form {:action "/admin/match/remove" :method "POST"}
+     (match-table-data matches players :admin true)]]))
