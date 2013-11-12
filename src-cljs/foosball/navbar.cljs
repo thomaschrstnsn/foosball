cac99ff74e3d5f1fefa4a213aa2ac72a55b6692b
Thomas Christensen <thomas@chrstnsn.dk>
2013-11-12 22:39:39 +0100

    redefined navbar structure (dropdown for statistics)

--- src-cljs/foosball/navbar.cljs
+++ src-cljs/foosball/navbar.cljs
@@ -3,7 +3,14 @@
   (:require [dommy.core :as dommy]
             [foosball.dom-utils :as u]))
 
-(defn set-active-navbar-by-id [id]
-  (let [el (sel1 id)]
-    (when el
-      (dommy/add-class! el :active))))
+(defn set-active-navbar-by-id
+  "Adds class active to the element with given id.
+   Also supports dropdown menus, such that if element has a parent li.dropdown it is also set active."
+  [id]
+  (let [el              (sel1 id)
+        dropdown-parent (->> (dommy/ancestor-nodes el)
+                             (filter (fn [e] (dommy/has-class? e :dropdown)))
+                             first)
+        set-active!     (fn [e] (when e (dommy/add-class! e :active)))]
+    (set-active! el)
+    (set-active! dropdown-parent)))
--- src/foosball/views/layout.clj
+++ src/foosball/views/layout.clj
@@ -9,26 +9,38 @@
             [cemerick.austin.repls :refer (browser-connected-repl-js)]))
 
 (defn header []
-  [:div.navbar.navbar-static-top.navbar-default
-   [:a.navbar-brand {:href "/"} "Foosball"]
-   [:ul.nav.navbar-nav.pull-left
-    (when (auth/user?)
-      (list [:li#nav-matchup (link-to "/matchup" "Matchup")]
-            [:li#nav-report  (link-to "/report/match" "Report Match Result")]))
-    [:li#nav-players-stats (link-to "/stats/players" "Player Statistics")]
-    [:li#nav-teams-stats (link-to "/stats/teams" "Team Statistics")]
-    [:li#nav-matches (link-to "/matches" "Played Matches")]
-    [:li#nav-player-log (link-to "/player/log" "Player Log")]
-    (when (auth/admin?)
-      [:li#nav-admin (link-to "/admin" "Admin")])
-    [:li#nav-about {:title (str "Version " (:version project))} (link-to "/about" "About")]]
-   [:ul.nav.navbar-nav.pull-right
-    (let [playername (auth/current-auth :playername)]
-      [:li (if-not (auth/current-auth)
-             (auth/login-form  :form-class "navbar-form")
-             (auth/logout-form :extra-class "navbar-form"
-                               :text (str "Logout")
-                               :title playername))])]])
+  (let [a-matchup        (link-to "/matchup"       "Matchup")
+        a-report         (link-to "/report/match"  "Report Match Result")
+        a-stats-player   (link-to "/stats/players" "Player Statistics")
+        a-stats-team     (link-to "/stats/teams"   "Team Statistics")
+        a-player-log     (link-to "/player/log"    "Player Log")
+        a-played-matches (link-to "/matches"       "Played Matches")
+        a-admin          (link-to "/admin"         "Admin")
+        a-about          (link-to "/about"         "About")]
+    [:div.navbar.navbar-static-top.navbar-default
+     [:a.navbar-brand {:href "/"} "Foosball"]
+     [:ul.nav.navbar-nav.pull-left
+      (when (auth/user?)
+        (list [:li#nav-matchup a-matchup]
+              [:li#nav-report a-report]))
+      [:li.dropdown
+       [:a.dropdown-toggle {:href "#" :data-toggle "dropdown"} "Statistics" [:b.caret]]
+       [:ul.dropdown-menu
+        [:li#nav-players-stats a-stats-player]
+        [:li#nav-teams-stats   a-stats-team]
+        [:li.divider]
+        [:li#nav-player-log    a-player-log]]]
+      [:li#nav-matches a-played-matches]
+      (when (auth/admin?)
+        [:li#nav-admin a-admin])
+      [:li#nav-about {:title (str "Version " (:version project))} a-about]]
+     [:ul.nav.navbar-nav.pull-right
+      (let [playername (auth/current-auth :playername)]
+        [:li (if-not (auth/current-auth)
+               (auth/login-form  :form-class "navbar-form")
+               (auth/logout-form :extra-class "navbar-form"
+                                 :text (str "Logout")
+                                 :title playername))])]]))
 
 (defn footer [auto-refresh?]
   (list
