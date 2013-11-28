ed72b6c087694c73dc094b6e0c1979d12f5f58c5
Thomas Christensen <thomas@chrstnsn.dk>
2013-11-28 07:04:23 +0100

    pretty up migrations

--- src/foosball/models/migration.clj
+++ src/foosball/models/migration.clj
@@ -4,7 +4,9 @@
   (:use [taoensso.timbre :only [trace debug info warn error fatal spy]])
   (:require [foosball.util :as util]))
 
-(defn- ensure-players-users-have-roles [conn]
+(defn- ensure-players-users-have-roles
+  "Ensure every player has the default role"
+  [conn]
   (let [dbc                (db conn)
         players-with-names (->> (d/q '[:find ?pid :where [?pid :player/name _]] dbc)
                                 (map (fn [[id]] id))
@@ -14,7 +16,7 @@
                                 set)
         players-to-default (difference players-with-names players-with-roles)
         transaction        (map (fn [id] {:db/id id :user/role :user}) players-to-default)]
-    (info {:ensure-players-users-have-roles transaction})
+    (info "Players changed:" (count players-to-default))
     @(d/transact conn transaction)))
 
 (defn- get-leagues [dbc]
@@ -29,16 +31,19 @@
       (filter (fn [{:keys [name]}] (= name default-league-name)))
       first))
 
-(defn- ensure-atleast-one-league-exists [conn]
+(defn- ensure-atleast-one-league-exists
+  "Create a default league if one does not exist"
+  [conn]
   (let [dbc         (db conn)
         leagues     (get-leagues dbc)
         no-leagues? (empty? leagues)]
-    (info {:ensure-atleast-one-league-exists no-leagues?})
     (when no-leagues?
-      (info "creating league:" default-league-name)
+      (info "Creating default league:" default-league-name)
       @(d/transact conn [{:db/id (d/tempid :db.part/user) :league/name default-league-name}]))))
 
-(defn- ensure-all-players-have-atleast-one-league [conn]
+(defn- ensure-all-players-have-atleast-one-league
+  "Ensure that every player is in a league"
+  [conn]
   (let [dbc                      (db conn)
         default-league           (get-default-league dbc)
         players-with-names       (->> (d/q '[:find ?pid :where [?pid :player/name _]] dbc)
@@ -50,12 +55,12 @@
         players-to-give-a-league (difference players-with-names players-with-leagues)
         transaction              (map (fn [pid] {:db/id pid :player/leagues (:id  default-league)})
                                       players-to-give-a-league)]
-    (info (merge {:ensure-all-players-have-atleast-one-league players-to-give-a-league
-                  :count (count players-to-give-a-league)}
-                 (util/symbols-as-map default-league)))
+    (info "Players changed:" (count players-to-give-a-league))
     @(d/transact conn transaction)))
 
-(defn- ensure-all-matches-happened-in-a-league [conn]
+(defn- ensure-all-matches-happened-in-a-league
+  "Ensure that all matches have been played in a league"
+  [conn]
   (let [dbc                      (db conn)
         default-league           (get-default-league dbc)
         matches-played           (->> (d/q '[:find ?mid :where [?mid :match/time _]] dbc)
@@ -67,18 +72,16 @@
         matches-to-give-a-league (difference matches-played matches-with-leagues)
         transaction              (map (fn [mid] {:db/id mid :match/league (:id default-league)})
                                       matches-to-give-a-league)]
-    (info (merge {:ensure-all-matches-happened-in-a-league matches-to-give-a-league
-                  :count (count matches-to-give-a-league)}
-                 (util/symbols-as-map default-league)))
+    (info "Matches changed:" (count matches-to-give-a-league))
     @(d/transact conn transaction)))
 
 (def migrations "seq of idempotent migration functions"
-  [["ensure-players-users-have-roles" ensure-players-users-have-roles]
-   ["ensure-atleast-one-league-exists" ensure-atleast-one-league-exists]
-   ["ensure-all-players-have-atleast-one-league" ensure-all-players-have-atleast-one-league]
-   ["ensure-all-matches-happened-in-a-league" ensure-all-matches-happened-in-a-league]])
+  [#'ensure-players-users-have-roles
+   #'ensure-atleast-one-league-exists
+   #'ensure-all-players-have-atleast-one-league
+   #'ensure-all-matches-happened-in-a-league])
 
 (defn migrate-schema-and-data [conn]
-  (doseq [[name migration-fn] migrations]
-    (info "running migration:" name)
-    (migration-fn conn)))
+  (doseq [migration-var migrations]
+    (info "Running migration:" (:doc (meta migration-var)))
+    (migration-var conn)))
