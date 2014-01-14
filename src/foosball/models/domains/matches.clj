f245a7522a53dd8489825765c6a6c85c41950a58
Thomas Christensen <thomas@chrstnsn.dk>
2014-01-14 16:22:14 +0100

    optimize get-all players and matches db access
    from ~1 second for 377 matches to ~40ms

--- src/foosball/models/domains/matches.clj
+++ src/foosball/models/domains/matches.clj
@@ -19,48 +19,38 @@
                      {:db/id match-id :match/reported-by reported-by}]]
     (d/transact conn transaction)))
 
-(defn- get-player [dbc id]
-  (->> (d/q '[:find ?player :in $ ?id :where [?id :player/name ?player]] dbc id)
-       ffirst))
-
-(defn- get-team [dbc id]
-  (->> (d/q '[:find ?p1 ?p2 ?score
-              :in $ ?id :where
-              [?id :team/player1 ?p1]
-              [?id :team/player2 ?p2]
-              [?id :team/score ?score]] dbc id)
-       (map (fn [[p1 p2 score]] {:player1 (get-player dbc p1)
-                                :player2 (get-player dbc p2)
-                                :score score}))
-       first))
-
-(defn- get-reported-by [dbc matchid]
-  (->> (d/q '[:find ?n ?pid
-              :in $ ?matchid
-              :where
-              [?matchid :match/reported-by ?pid]
-              [?pid     :player/name       ?n]] dbc matchid)
-       (map (fn [[name id]] name))
-       first))
-
 (defn get-all [dbc]
-  (->> (d/q '[:find ?m ?mt ?t1 ?t2 ?tx
+  (->> (d/q '[:find ?m ?mt
+              ?t1id ?t1p1 ?t1p2 ?t1score
+              ?t2id ?t2p1 ?t2p2 ?t2score
+              ?tx
               :in $
               :where
               [?m :match/time  ?mt ?tx]
-              [?m :match/team1 ?t1]
-              [?m :match/team2 ?t2]
-              [?t1 :team/player1 _]
-              [?t1 :team/player2 _]
-              [?t1 :team/score   _]
-              [?t2 :team/player1 _]
-              [?t2 :team/player2 _]
-              [?t2 :team/score   _]] dbc)
-       (map (fn [[mid mt t1 t2 tx]]
-              {:id mid :matchdate mt :tx tx
-               :team1 (merge {:id t1} (get-team dbc t1))
-               :team2 (merge {:id t2} (get-team dbc t2))
-               :reported-by (get-reported-by dbc mid)}))
+              [?m :match/team1 ?t1id]
+              [?m :match/team2 ?t2id]
+
+              [?t1id :team/player1 ?t1p1id]
+              [?t1p1id :player/name ?t1p1]
+              [?t1id :team/player2 ?t1p2id]
+              [?t1p2id :player/name ?t1p2]
+              [?t1id :team/score   ?t1score]
+
+              [?t2id :team/player1 ?t2p1id]
+              [?t2p1id :player/name ?t2p1]
+              [?t2id :team/player2 ?t2p2id]
+              [?t2p2id :player/name ?t2p2]
+              [?t2id :team/score   ?t2score]] dbc)
+       (map (fn [[mid mt
+                 t1id t1p1 t1p2 t1score
+                 t2id t2p1 t2p2 t2score
+                 tx]]
+              (let [match (d/entity dbc mid)
+                    reporter (d/entity dbc (get-in match [:match/reported-by :db/id] ))]
+                {:id mid :matchdate mt :tx tx
+                 :team1 {:id t1id :player1 t1p1 :player2 t1p2 :score t1score}
+                 :team2 {:id t2id :player1 t2p1 :player2 t2p2 :score t2score}
+                 :reported-by (:player/name reporter)})))
        (sort-by (juxt :matchdate :tx))))
 
 (defn delete! [conn id]
