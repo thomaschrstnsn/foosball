f10c54c984fdfe34dae1d97f254ba5b6536ddcb2
Thomas Christensen <thomas@chrstnsn.dk>
2014-04-16 18:01:05 +0200

    update misc. project dependencies

--- project.clj
+++ project.clj
@@ -2,27 +2,27 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.datomic/datomic-free "0.8.4159" :exclusions [com.amazonaws/aws-java-sdk]]
                  [ring/ring-core "1.2.0" :exclusions [org.clojure/tools.reader]]
-                 [org.clojure/data.json "0.2.3"]
-                 [liberator "0.10.0" :exlusions [org.clojure/data.json]]
+                 [org.clojure/data.json "0.2.4"]
+                 [liberator "0.11.0" :exlusions [org.clojure/data.json]]
                  [lib-noir "0.6.8"]
-                 [compojure "1.1.5" :exclusions [org.clojure/tools.macro
+                 [compojure "1.1.6" :exclusions [org.clojure/tools.macro
                                                  org.clojure/core.incubator
                                                  ring/ring-core]]
                  [ring-server "0.3.0" :exclusions [org.clojure/core.incubator]]
                  [com.cemerick/friend "0.2.0"]
-                 [com.taoensso/timbre "2.7.1"]
+                 [com.taoensso/timbre "3.1.6"]
                  [log4j "1.2.17" :exclusions [javax.mail/mail
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
                                               com.sun.jmx/jmxri]]
-                 [hiccup "1.0.4"]
-                 [org.clojure/math.combinatorics "0.0.4"]
+                 [hiccup "1.0.5"]
+                 [org.clojure/math.combinatorics "0.0.7"]
                  [clj-time "0.6.0"]
-                 [com.stuartsierra/component "0.2.0"]
+                 [com.stuartsierra/component "0.2.1"]
                  [org.clojure/tools.nrepl "0.2.3"]]
 
   :hooks [configleaf.hooks]
-  :configleaf {:verbose true}
+  :configleaf {:verbose false}
 
   :lein-release {:deploy-via :shell
                  :build-via  :lein-ring-uberwar
@@ -60,13 +60,13 @@
                    :dependencies [[org.clojure/clojurescript "0.0-2030"]
                                   [org.clojure/core.async "0.1.256.0-1bf8cf-alpha"]
                                   [org.clojure/tools.namespace "0.2.4"]
-                                  [org.clojure/java.classpath "0.2.1"]
+                                  [org.clojure/java.classpath "0.2.2"]
                                   [ring-mock "0.1.5"]
                                   [ring/ring-devel "1.2.0"]
-                                  [midje "1.5.1"]
+                                  [midje "1.5.1" :exclusions [org.codehaus.plexus/plexus-utils slingshot]]
                                   [server-socket "1.0.0"]
-                                  [prismatic/dommy "0.1.1"]
-                                  [secretary "0.4.0" :exclusions [org.clojure/clojurescript]]
+                                  [prismatic/dommy "0.1.2"]
+                                  [secretary "1.1.0" :exclusions [org.clojure/clojurescript]]
                                   [jayq "2.5.0"]]
                    :plugins      [[com.cemerick/austin "0.1.3"]]
                    :repositories {"sonatype-oss-public"
