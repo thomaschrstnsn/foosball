ebd2b1629a7846f3acdc3a951212525ba4f1edc6
Thomas Christensen <thomas@chrstnsn.dk>
2014-08-14 15:25:36 +0200

    add terminal-notify to phantom runner shell script

--- run-cljs-phantom.sh
+++ run-cljs-phantom.sh
@@ -1,6 +1,29 @@
 #! /bin/sh
 
-# TODO: use terminal-notifier if non-empty $1
-# echo "got extra parameters: '$1'"
+function notify {
+    terminal-notifier -title "$1" -message "$2"
+}
 
-phantomjs extern/test/runner-none.js target/cljs/test target/cljs/testable.js extern/test/bind-shim.js resources/public/js/extern/react-0.9.0.js skipRootBind=true
+function phantom_test_runner {
+    CLJSTEST="clojurescript.test"
+    phantomjs extern/test/runner-none.js target/cljs/test target/cljs/testable.js extern/test/bind-shim.js resources/public/js/extern/react-0.9.0.js skipRootBind=true
+    if [[ $? -eq 0 ]];
+    then
+        notify $CLJSTEST "success"
+    else
+        notify $CLJSTEST "fail"
+    fi
+}
+
+if [[ $# -eq 1 ]];
+then
+    notify "cljsbuild" $1
+    if [[ $1 == *Successfully* ]]
+    then
+        phantom_test_runner
+    else
+        echo Error: during cljsbuild
+    fi
+else
+    phantom_test_runner
+fi;
